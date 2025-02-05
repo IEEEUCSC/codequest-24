99b8886f3763e25429980f9d6a721035a7a263dc
dinil-thilakarathne <sonacode44@gmail.com>
2025-02-05 14:07:56 +0530

    feat: add navigation links and site title to data module

---
+++ libs/data.ts
@@ -0,0 +1,9 @@
+export const NAV_LINKS = [
+  { href: "#competition", label: "Competition" },
+  { href: "#timeline", label: "Timeline" },
+  { href: "#prizes", label: "Prizes" },
+  { href: "#sponsors", label: "Sponsors" },
+  { href: "#faqs", label: "FAQ" },
+];
+
+export const SITE_TITLE = "CodeQuest: Vault Edition";
