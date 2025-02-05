6ef8f10ec6f5226c811b7fa326ec5c192b903da6
dinil-thilakarathne <sonacode44@gmail.com>
2025-02-05 14:08:18 +0530

    feat: add Hero and HomeSec components for enhanced landing page experience

---
+++ components/Hero/index.tsx
@@ -0,0 +1,24 @@
+import { SITE_TITLE } from "@/libs/data";
+
+const Hero = () => {
+  return (
+    <div className="hero container mx-auto flex-grow  flex flex-col justify-center items-center text-white">
+      <div className="flex flex-col items-center gap-y-3 lg:gap-y-5">
+        <h3 className="text-center text-lg lg:text-2xl">
+          IEEE STUDENT BRANCH OF <br />
+          UNIVERSITY OF COLOMBO SCHOOL OF COMPUTING <br />
+          PROUDLY PRESENTS
+        </h3>
+        <h1 className=" text-4xl lg:text-6xl xl:text-7xl 2xl:text-8xl">
+          {SITE_TITLE}
+        </h1>
+        <p className="text-balance text-center text-sm lg:max-w-[65ch] lg:text-base">
+          A Capture the Flag (CTF) hackathon aimed at enhancing cybersecurity
+          skills among undergraduates.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ components/Home/index.tsx
@@ -0,0 +1,13 @@
+import Header from "../Header";
+import Hero from "../Hero";
+
+const HomeSec = () => {
+  return (
+    <div className=" border-white hero-bg min-h-screen flex flex-col">
+      <Header />
+      <Hero />
+    </div>
+  );
+};
+
+export default HomeSec;
