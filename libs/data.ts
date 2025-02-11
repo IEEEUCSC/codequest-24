d4f637c677fa54041d3fb8189c6e770e7b60f292
dinil-thilakarathne <sonacode44@gmail.com>
2025-02-11 09:52:33 +0530

    feat: add social media links with icons to data module

--- libs/data.ts
+++ libs/data.ts
@@ -1,3 +1,7 @@
+import React from "react";
+
+import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa6";
+
 export const NAV_LINKS = [
   { href: "#competition", label: "Competition" },
   { href: "#timeline", label: "Timeline" },
@@ -7,3 +11,28 @@ export const NAV_LINKS = [
 ];
 
 export const SITE_TITLE = "CodeQuest: Vault Edition";
+
+
+interface SocialMediaLink {
+  name: string;
+  link: string;
+  icon: React.ReactNode;
+}
+
+export const socialMediaLinks: SocialMediaLink[] = [
+  {
+    name: "Facebook",
+    link: "https://facebook.com/IEEECSUCSC ",
+    icon: React.createElement(FaFacebook),
+  },
+  {
+    name: "Instagram",
+    link: "https://www.instagram.com/ieee.cs.ucsc/ ",
+    icon: React.createElement(FaInstagram),
+  },
+  {
+    name: "LinkedIn",
+    link: "https://www.linkedin.com/company/ieee-computer-society-student-branch-chapter-of-ucsc",
+    icon: React.createElement(FaLinkedin),
+  },
+];
