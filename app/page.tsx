495e8facbdb3e2cf3395089a4b168e4e94cfa797
dinil-thilakarathne <sonacode44@gmail.com>
2025-02-13 17:35:52 +0530

    refactor: clean up unused imports and remove test page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
 import AnimatedGridWrapper from "@/components/AnimatedGrid/AnimatedGridWrapper";
 import Hero from "@/components/Hero";
-import Contacts from "@/components/Contacts";
+// import Contacts from "@/components/Contacts";
 
 export default function Home() {
   return (
     <div className="">
-      {/* <AnimatedGridWrapper /> */}
+      <AnimatedGridWrapper />
       <Hero />
-      <Contacts />
-      {/* <div className="min-h-screen w-full" />
-      <div className="min-h-screen w-full" /> */}
+      {/* <Contacts /> */}
+      <div className="min-h-screen w-full" />
+      <div className="min-h-screen w-full" />
     </div>
   );
 }
