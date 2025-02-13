a182f0b66f42849b557b5f6f2c9601aba3994a3c
dinil-thilakarathne <sonacode44@gmail.com>
2025-02-13 17:34:36 +0530

    feat: update contact information structure and add new images

--- libs/data.ts
+++ libs/data.ts
@@ -1,3 +1,4 @@
+import { StaticImageData } from "next/image";
 import React from "react";
 
 import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa6";
@@ -11,7 +12,6 @@ export const NAV_LINKS = [
 ];
 
 export const SITE_TITLE = "CodeQuest: Vault Edition";
-
 
 interface SocialMediaLink {
   name: string;
@@ -34,5 +34,43 @@ export const socialMediaLinks: SocialMediaLink[] = [
     name: "LinkedIn",
     link: "https://www.linkedin.com/",
     icon: React.createElement(FaLinkedin),
+  },
+];
+
+// contact persons data
+export interface ContactInfo {
+  name: string;
+  title: string;
+  email: string;
+  phone: string;
+  imageSrc: string | StaticImageData;
+}
+
+// contact persons images
+// import person1 from "@/assets/contacts/a.avif";
+import person2 from "@/assets/contacts/images.jpg";
+import person3 from "@/assets/contacts/img.jpg";
+
+export const contactInfo: ContactInfo[] = [
+  {
+    name: "John Doe",
+    title: "Vice President",
+    email: "john.doe@example.com",
+    phone: "123-456-7890",
+    imageSrc: person2,
+  },
+  {
+    name: "Jane Smith",
+    title: "Secretary",
+    email: "jane.smith@example.com",
+    phone: "987-654-3210",
+    imageSrc: person2,
+  },
+  {
+    name: "Alice Johnson",
+    title: "Treasurer",
+    email: "alice.johnson@example.com",
+    phone: "555-123-4567",
+    imageSrc: person3,
   },
 ];
