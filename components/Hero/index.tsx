c06f016fa75faf6541f05697e7ae303d482cea29
dinil-thilakarathne <sonacode44@gmail.com>
2025-02-18 19:43:33 +0530

    style: update Hero component with SVG assets and responsive background image

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -3,8 +3,11 @@
 import { useEffect, useState } from "react";
 import Image from "next/image";
 
-import HeroHeader from "@/public/hero-header.png";
+import HeroHeader from "@/assets/hero-header.svg";
 import { CountdownTimer } from "../CountdownTimer";
+import { useMediaQuery } from "@/hooks/useMediaQuery";
+import { useComponentDimensions } from "@/hooks/useComponentDimensions";
+import HeroBgImg from "@/assets/hero-bg.svg";
 
 const Hero = () => {
   const [targetDate, setTargetDate] = useState(new Date());
@@ -15,11 +18,27 @@ const Hero = () => {
     setTargetDate(date);
   }, []);
 
+  const desktop = useMediaQuery("(min-width: 1024px)");
+
+  const dimensions = useComponentDimensions("register-button");
+
+  const [heroLineRight, setHeroLineRight] = useState(0);
+  const heroLineWidth = 48;
+
+  useEffect(() => {
+    setHeroLineRight(dimensions.width / 2);
+  }, [dimensions, heroLineRight]);
+
   return (
     <div className="hero hero-bg relative min-h-svh overflow-hidden text-white">
       <div className="relative container mx-auto flex min-h-screen flex-col items-center justify-between">
-        <div className="bg-primary-400 background-glow top-element absolute top-8 right-[70px] block h-svh w-[48px]" />
-        <div className="top-element flex h-[calc(100%-125px)] grow flex-col items-center justify-center gap-y-3 pt-[85px] lg:gap-y-6 xl:h-auto">
+        {desktop && (
+          <div
+            className="bg-primary-400 background-glow top-element absolute top-8 block h-svh"
+            style={{ width: heroLineWidth, right: 280 / 2 - heroLineWidth / 2 }}
+          />
+        )}
+        <div className="top-element flex h-[calc(100%-125px)] grow flex-col items-center justify-center gap-y-3 pt-[85px] lg:gap-y-8 xl:h-auto">
           <h3 className="text-center text-base md:text-lg lg:text-2xl">
             IEEE STUDENT BRANCH OF <br />
             UNIVERSITY OF COLOMBO SCHOOL OF COMPUTING <br />
@@ -37,6 +56,14 @@ const Hero = () => {
           </p>
           <CountdownTimer targetDate={targetDate} title="Event starts in" />
         </div>
+      </div>
+      <div className="absolute bottom-0 h-full w-full">
+        <Image
+          src={HeroBgImg}
+          alt="Hero background"
+          layout="fill"
+          objectFit="cover"
+        />
       </div>
     </div>
   );
