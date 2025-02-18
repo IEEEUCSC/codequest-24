fd1920e322f7524df84b77395b1fb1689c6381db
dinil-thilakarathne <sonacode44@gmail.com>
2025-02-18 19:45:49 +0530

    feat: enhance CountdownTimer styling and layout for improved visibility

--- components/CountdownTimer/index.tsx
+++ components/CountdownTimer/index.tsx
@@ -11,8 +11,10 @@ export function CountdownTimer({ targetDate, title }: CountdownTimerProps) {
   const timeLeft = useCountdown(targetDate);
 
   return (
-    <div className="mx-auto w-full max-w-[300px] sm:max-w-xl rounded-xl  glow-shadow bg-dark-200  border-primary-200 border-4 px-6 py-2 text-center">
-      <h2 className="font-consola mb-4 text-lg lg:text-2xl">{title}</h2>
+    <div className="countdown-timer-bg bg-dark-200 border-primary-400 mx-auto w-full max-w-[300px] rounded-xl border-6 px-6 py-2 text-center sm:max-w-xl">
+      <h2 className="font-consola text-primary-50 mb-4 text-lg lg:text-2xl">
+        {title}
+      </h2>
       <div className="flex items-center justify-center gap-4">
         <TimeUnit value={timeLeft.days} label="DD" />
         <Separator />
@@ -31,7 +33,7 @@ function TimeUnit({ value, label }: { value: number; label: string }) {
 
   return (
     <div className="flex flex-col items-center">
-      <span className="font-consola text-xl lg:text-4xl font-bold tabular-nums">
+      <span className="font-consola text-xl font-bold tabular-nums lg:text-4xl">
         {formattedValue}
       </span>
       <span className="mt-2 text-sm">{label}</span>
@@ -40,5 +42,5 @@ function TimeUnit({ value, label }: { value: number; label: string }) {
 }
 
 function Separator() {
-  return <div className="mx-2 h-12 w-px bg-primary-400" />;
+  return <div className="bg-primary-400 mx-2 h-12 w-px" />;
 }
