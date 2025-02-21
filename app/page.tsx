76da949c209e77084120cdeb3a32c7f8cda596cc
dinil-thilakarathne <sonacode44@gmail.com>
2025-02-22 03:00:14 +0530

    feat: enable Timeline and FAQs components in Home page for improved content display

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
 import AnimatedGridWrapper from "@/components/AnimatedGrid/AnimatedGridWrapper";
 import Hero from "@/components/Hero";
-// import FAQs from "@/components/FAQs";
-// import TimelineSection from "@/components/Timeline";
+import FAQs from "@/components/FAQs";
+import TimelineSection from "@/components/Timeline";
 // import Contacts from "@/components/Contacts";
 
 export default function Home() {
   return (
-    <div className=" bg-dark-300">
+    <div className="bg-dark-300">
       <AnimatedGridWrapper />
       <Hero />
       {/* <Contacts /> */}
-      {/* <TimelineSection />
-      <FAQs /> */}
+      <TimelineSection />
+      <FAQs />
       <div className="min-h-screen w-full" />
       <div className="min-h-screen w-full" />
     </div>
