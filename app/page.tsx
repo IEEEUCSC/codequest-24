595e57f63227dd0d35a34008fb9b99e64fc8a124
dinil-thilakarathne <sonacode44@gmail.com>
2025-02-27 00:08:47 +0530

    feat: comment out unused components in Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import AnimatedGridWrapper from "@/components/AnimatedGrid/AnimatedGridWrapper";
 import Hero from "@/components/Hero";
-import FAQs from "@/components/FAQs";
-import TimelineSection from "@/components/Timeline";
+// import FAQs from "@/components/FAQs";
+// import TimelineSection from "@/components/Timeline";
+// import Sponsors from "@/components/Sponsors";
 // import Contacts from "@/components/Contacts";
 
 export default function Home() {
@@ -10,8 +11,9 @@ export default function Home() {
       <AnimatedGridWrapper />
       <Hero />
       {/* <Contacts /> */}
-      <TimelineSection />
-      <FAQs />
+      {/* <TimelineSection /> */}
+      {/* <Sponsors /> */}
+      {/* <FAQs /> */}
       <div className="min-h-screen w-full" />
       <div className="min-h-screen w-full" />
     </div>
