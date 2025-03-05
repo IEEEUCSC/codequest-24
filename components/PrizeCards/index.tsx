1dd551eb36fd08504ad56cd276e7d903c82d84ab
dinil-thilakarathne <sonacode44@gmail.com>
2025-03-06 02:20:07 +0530

    feat: refactor PrizeCards component to use CardsData array for rendering cards

--- components/PrizeCards/index.tsx
+++ components/PrizeCards/index.tsx
@@ -1,29 +1,36 @@
+import { CardProps } from "@/libs/types";
 import SectionHeader from "../SectionHeader";
 import Card from "./Card";
 
+const CardsData: CardProps[] = [
+  {
+    position: "2nd",
+    amount: "40,000",
+    className: "card-glow h-[330px] w-[280px] md:translate-y-8",
+  },
+  {
+    position: "1st",
+    amount: "60,000",
+    className: "card-glow z-10 h-[410px] w-[320px]",
+  },
+  {
+    position: "3rd",
+    amount: "20,000",
+    className: "card-glow h-[330px] w-[280px] md:translate-y-8",
+  },
+];
+
 const PrizeCards = () => {
   return (
-    <div className="main-section top-element relative min-h-[300px] w-full px-4 py-8">
+    <div
+      className="main-section top-element relative min-h-[300px] w-full px-4 py-8"
+      id="prizes"
+    >
       <SectionHeader title="Prizes" />
-      <div className="flex flex-col items-center justify-center gap-8 md:flex-row sec-inner-y">
-        {/* 2nd Place - Slightly smaller */}
-        <Card
-          position="2nd"
-          amount="40,000"
-          className="card-glow h-[330px] w-[280px] md:translate-y-8"
-        />
-        {/* 1st Place - Largest */}
-        <Card
-          position="1st"
-          amount="60,000"
-          className="card-glow z-10 h-[410px] w-[320px]"
-        />
-        {/* 3rd Place - Slightly smaller */}
-        <Card
-          position="3rd"
-          amount="20,000"
-          className="card-glow h-[330px] w-[280px] md:translate-y-8"
-        />
+      <div className="sec-inner-y flex flex-col items-center justify-center gap-8 md:flex-row">
+        {CardsData.map((card, index) => (
+          <Card key={index} {...card} />
+        ))}
       </div>
     </div>
   );
