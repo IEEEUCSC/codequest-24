203f294096385b8b625d6997859340c646f5b8af
dinil-thilakarathne <sonacode44@gmail.com>
2025-04-13 10:59:55 +0530

    refactor: simplify ProfileCard component and remove unused props from contactData

--- components/Contacts/ProfileCard/index.tsx
+++ components/Contacts/ProfileCard/index.tsx
@@ -1,111 +1,69 @@
 "use client";
-
-import { useState } from "react";
 import Image from "next/image";
 
 import { ContactDataProps } from "@/libs/contactData";
-import { useMediaQuery } from "@/hooks/useMediaQuery";
-import { cn } from "@/libs/utils";
+import Glassmorphism from "@/components/Glassmorphism";
+import { motion } from "motion/react";
 
-interface ProfileCardProps {
-  contact: ContactDataProps;
-  onMouseEnter: (contact: ContactDataProps) => void;
-  onMouseLeave: () => void;
-  onMouseClick: (contact: ContactDataProps) => void;
-}
+type ProfileCardProps = ContactDataProps & {
+  index: number;
+};
 
 const ProfileCard: React.FC<ProfileCardProps> = ({
-  contact,
-  onMouseEnter,
-  onMouseLeave,
-  onMouseClick,
-}) => {
-  const [isHovered, setIsHovered] = useState(false);
-  const desktop = useMediaQuery("(min-width: 768px");
-
-  // Handler functions for mouse events
-  const handleMouseEnter = () => {
-    setIsHovered(true);
-    onMouseEnter(contact);
-  };
-
-  const handleMouseCLick = () => {
-    onMouseClick(contact);
-  };
-
-  const handleMouseLeave = () => {
-    setIsHovered(false);
-    onMouseLeave();
-  };
-
-  // Skip rendering the data cell position - we'll render that separately
-  if (
-    contact.position.gridColumn === "1 / span 2" &&
-    contact.position.gridRow === "2"
-  ) {
-    return null;
-  }
-
-  const gridColumnClass = desktop
-    ? contact.position.gridColumn === "3"
-      ? "col-start-3"
-      : ""
-    : "";
-  const gridRowClass = desktop
-    ? contact.position.gridRow === "2"
-      ? "row-start-2"
-      : ""
-    : "";
+  index,
+  ...props
+}: ProfileCardProps) => {
+  const { image, name, role, email, phone } = props;
 
   return (
-    <div
-      className={cn(
-        "relative w-full overflow-hidden rounded bg-red-600 ease-in-out hover:cursor-pointer md:pt-[100%]",
-        gridColumnClass,
-        gridRowClass,
-      )}
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-      onClick={handleMouseCLick}
+    <motion.div
+      variants={fadeUpVariant}
+      initial="initial"
+      whileInView="animate"
+      viewport={{ once: false, amount: 0.2 }}
+      custom={index}
     >
-      <div className="md:absolute md:inset-0">
-        <div className="xs:grid-cols-2 relative grid h-full w-full md:grid-cols-none">
-          <div className="relative min-h-[280px] md:min-h-0">
-            <Image
-              src={contact.image}
-              alt={`${contact.name}`}
-              fill
-              style={{ objectFit: "cover" }}
-              className="h-full w-full object-cover"
-            />
-          </div>
-
-          {!desktop && (
-            <div className="flex flex-col gap-y-1 p-4">
-              <h2 className="text-xl font-bold text-white">{contact.name}</h2>
-              <p className="text-white">{contact.role}</p>
-
-              <div className="text-sm text-white">
-                <span className="mr-1 font-bold">Email:</span>
-                <span>{contact.email}</span>
+      <Glassmorphism className="rounded">
+        <div className="bg-primary-400/40 absolute top-0 left-0 h-full w-full mask-radial-[100%_100%] mask-radial-from-60% mask-radial-to-100% mask-radial-at-bottom-left" />
+        <div className="relative w-full overflow-hidden rounded ease-in-out">
+          <div className="">
+            <div className="xs:grid-cols-2 relative grid h-full w-full md:grid-cols-1">
+              <div className="relative min-h-[280px]">
+                <Image
+                  src={image}
+                  alt={`${name}`}
+                  fill
+                  style={{ objectFit: "cover" }}
+                  className="h-full w-full object-cover"
+                />
               </div>
-
-              <div className="text-sm text-white">
-                <span className="mr-1 font-bold">Phone:</span>
-                <span>{contact.phone}</span>
+              <div className="flex flex-col gap-y-1 p-4">
+                <h2 className="text-xl font-bold text-white">{name}</h2>
+                <p className="text-white">{role}</p>
+                <div className="text-sm text-white">
+                  <span className="mr-1 font-bold">Email:</span>
+                  <span>{email}</span>
+                </div>
+                <div className="text-sm text-white">
+                  <span className="mr-1 font-bold">Phone:</span>
+                  <span>{phone}</span>
+                </div>
               </div>
             </div>
-          )}
-
-          {desktop && (
-            <div
-              className={`bg-opacity-85 absolute inset-0 flex flex-col items-center justify-center bg-red-600 transition-opacity duration-300 ${isHovered ? "opacity-50" : "opacity-0"}`}
-            />
-          )}
+          </div>
         </div>
-      </div>
-    </div>
+      </Glassmorphism>
+    </motion.div>
   );
 };
 
 export default ProfileCard;
+
+const fadeUpVariant = {
+  initial: { opacity: 0, y: 20 },
+  animate: (i: number) => ({
+    opacity: 1,
+    y: 0,
+    transition: { delay: i * 0.1 },
+  }),
+};
--- components/Contacts/index.tsx
+++ components/Contacts/index.tsx
@@ -1,53 +1,20 @@
 "use client";
 
-import { useState } from "react";
-
 import ProfileCard from "./ProfileCard";
-import DataCell from "./DataCell";
 import contactData from "@/libs/contactData";
 import SectionHeader from "../SectionHeader";
-import { useMediaQuery } from "@/hooks/useMediaQuery";
 
 const TeamGrid: React.FC = () => {
   // State to track which contact is being hovered on
-  const [hoveredContactIndex, setHoveredContactIndex] = useState<number>(1);
-
-  const desktop = useMediaQuery("(min-width: 768px");
-
-  // Handler for when mouse enters a profile card
-  const handleHover = (id: number) => {
-    setHoveredContactIndex(id);
-  };
-
-  const handleMouseClick = (id: number) => {
-    setHoveredContactIndex(id);
-  };
-
-  // Handler for when mouse leaves a profile card
-  const handleHoverEnd = () => {
-    setHoveredContactIndex(1); // set default to first contact
-  };
-
-  // Determine which contact to display in the data cell
-  const displayedContact =
-    contactData.find((contact) => contact.id === hoveredContactIndex) ||
-    contactData[0]; // provide a default value
 
   return (
     <div className="main-section top-element">
       <SectionHeader title="Our Team" />
       <div className="px-1.5 md:px-0">
-        <div className="sec-inner-y bg-dark-300 top-element border-primary-400 relative grid w-full max-w-6xl gap-2 rounded-lg border-2 p-2 md:mx-auto md:grid-cols-3 md:grid-rows-2">
-          {contactData.map((contact) => (
-            <ProfileCard
-              key={contact.id}
-              contact={contact}
-              onMouseEnter={() => handleHover(contact.id)}
-              onMouseLeave={handleHoverEnd}
-              onMouseClick={() => handleMouseClick(contact.id)}
-            />
+        <div className="sec-inner-y bg-dark-300 top-element border-primary-400 relative grid w-full max-w-6xl gap-2 rounded-lg border-2 p-2 md:mx-auto md:grid-cols-2 md:grid-rows-2 lg:grid-cols-4 lg:grid-rows-1">
+          {contactData.map((contact, i) => (
+            <ProfileCard key={contact.id} {...contact} index={i} />
           ))}
-          {desktop && <DataCell contact={displayedContact} />}
         </div>
       </div>
     </div>
