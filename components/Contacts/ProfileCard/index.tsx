732b73ae6b9c54aae11b1dc3491c66e6ffe8c1ff
dinil-thilakarathne <sonacode44@gmail.com>
2025-04-18 06:42:21 +0530

    refactor: remove unused DataCell component and update TeamGrid section header

--- components/Contacts/ProfileCard/index.tsx
+++ components/Contacts/ProfileCard/index.tsx
@@ -20,34 +20,32 @@ const ProfileCard: React.FC<ProfileCardProps> = ({
       variants={fadeUpVariant}
       initial="initial"
       whileInView="animate"
-      viewport={{ once: false, amount: 0.2 }}
+      viewport={{ once: true, amount: 0.2 }}
       custom={index}
     >
       <Glassmorphism className="rounded">
         <div className="bg-primary-400/40 absolute top-0 left-0 h-full w-full mask-radial-[100%_100%] mask-radial-from-60% mask-radial-to-100% mask-radial-at-bottom-left" />
         <div className="relative w-full overflow-hidden rounded ease-in-out">
-          <div className="">
-            <div className="xs:grid-cols-2 relative grid h-full w-full md:grid-cols-1">
-              <div className="relative min-h-[280px]">
-                <Image
-                  src={image}
-                  alt={`${name}`}
-                  fill
-                  style={{ objectFit: "cover" }}
-                  className="h-full w-full object-cover"
-                />
+          <div className="xs:grid-cols-2 relative grid h-full w-full md:grid-cols-1">
+            <div className="relative min-h-[320px]">
+              <Image
+                src={image}
+                alt={`${name}`}
+                fill
+                style={{ objectFit: "cover" }}
+                className="h-full w-full object-cover object-center"
+              />
+            </div>
+            <div className="flex flex-col gap-y-1 p-4">
+              <h2 className="text-xl font-bold text-white">{name}</h2>
+              <p className="text-white">{role}</p>
+              <div className="text-sm text-white">
+                <span className="mr-1 font-bold">Email:</span>
+                <span>{email}</span>
               </div>
-              <div className="flex flex-col gap-y-1 p-4">
-                <h2 className="text-xl font-bold text-white">{name}</h2>
-                <p className="text-white">{role}</p>
-                <div className="text-sm text-white">
-                  <span className="mr-1 font-bold">Email:</span>
-                  <span>{email}</span>
-                </div>
-                <div className="text-sm text-white">
-                  <span className="mr-1 font-bold">Phone:</span>
-                  <span>{phone}</span>
-                </div>
+              <div className="text-sm text-white">
+                <span className="mr-1 font-bold">Phone:</span>
+                <span>{phone}</span>
               </div>
             </div>
           </div>
