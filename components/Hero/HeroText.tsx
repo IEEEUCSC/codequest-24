418c78ad69e50a0c33971f3a5488de40459322c0
dinil-thilakarathne <sonacode44@gmail.com>
2025-04-18 06:43:15 +0530

    refactor: replace static header with TypewriterEffect for dynamic text display

--- components/Hero/HeroText.tsx
+++ components/Hero/HeroText.tsx
@@ -1,22 +1,33 @@
 "use client";
 
+import { TypewriterEffect } from "../TextEffect";
+
 export const HeroSecHeader = () => {
+  const text = [
+    "IEEE STUDENT BRANCH OF",
+    "UNIVERSITY OF COLOMBO SCHOOL OF COMPUTING",
+    "PROUDLY PRESENTS",
+  ];
+
   return (
-    <h3
-      className="flex flex-col text-center text-base md:text-lg lg:text-2xl"
-      onCopy={(e) => {
-        e.preventDefault();
-        e.clipboardData.setData(
-          "text/plain",
-          "IEEE STUDENT BRANCH OF UNIVERSITY OF COLOMBO SCHOOL OF COMPUTING PROUDLY PRESENTS",
-        );
-      }}
-    >
-      IEEE STUDENT BRANCH OF
-      <br aria-hidden />
-      UNIVERSITY OF COLOMBO SCHOOL OF COMPUTING
-      <br aria-hidden />
-      PROUDLY PRESENTS
-    </h3>
+    <div className="flex flex-col text-center text-base will-change-[height,_contents] md:text-lg lg:text-2xl">
+      {text.map((line, index) => (
+        <TypewriterEffect
+          key={index}
+          text={line}
+          textClassName="text-base md:text-lg lg:text-2xl"
+          animationDelay={0.05}
+          startAnimationDelay={index * 0.5}
+          onCopy={(e) => {
+            e.preventDefault();
+            e.clipboardData.setData("text/plain", line);
+          }}
+          aria-label={line}
+          aria-description="This text is animated with a typewriter effect"
+          role="text"
+
+        />
+      ))}
+    </div>
   );
 };
