e47a72d5deae4dcd536d4fdd62fea305d2a567ba
Savith Panamgama <geethikasavith@gmail.com>
2025-04-20 13:58:49 +0530

    fix: update registration button text to "Coming Soon" and disable link for improved user experience

--- components/Header/RegBtn/index.tsx
+++ components/Header/RegBtn/index.tsx
@@ -3,11 +3,17 @@ import React from "react";
 import { BsArrowUpRightCircle } from "react-icons/bs";
 const RegButton = () => {
   return (
+    // <AnimatedButton
+    //   text="Register Now"
+    //   className="flex w-fit items-center gap-x-2 rounded-full px-4 py-3"
+    //   id="register-button"
+    //   to=""
+    // >
     <AnimatedButton
-      text="Register Now"
-      className="flex w-fit items-center gap-x-2 rounded-full px-4 py-3"
+      text="Coming Soon"
+      className="flex w-fit cursor-not-allowed items-center gap-x-2 rounded-full px-4 py-3 opacity-70"
       id="register-button"
-      to="https://tally.so/r/wkrR1o"
+      to="#"
     >
       <BsArrowUpRightCircle className="text-[20px]" />
     </AnimatedButton>
