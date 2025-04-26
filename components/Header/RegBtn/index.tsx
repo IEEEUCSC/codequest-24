7dee5a60005ede2f284980aa00322c1ec26d34db
Dinil Thilakarathne <126305123+Dinil-Thilakarathne@users.noreply.github.com>
2025-04-27 00:29:06 +0530

    feat: correct registration button text

--- components/Header/RegBtn/index.tsx
+++ components/Header/RegBtn/index.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const RegButton = () => {
   return (
     <AnimatedButton
-      text="Register Closed"
+      text="Registration Closed"
       className="flex w-fit items-center gap-x-2 rounded-full px-4 py-3 cursor-not-allowed"
       id="register-button"
     />
