7b84db2abfb1ea20b25ec8ee48bf61659b38d0a7
Dinil Thilakarathne <126305123+Dinil-Thilakarathne@users.noreply.github.com>
2025-05-02 23:51:13 +0530

    feat: feat registration closed

--- components/Header/RegBtn/index.tsx
+++ components/Header/RegBtn/index.tsx
@@ -1,18 +1,13 @@
 import AnimatedButton from "@/components/AnimatedButton";
 import React from "react";
-import { BsArrowUpRightCircle } from "react-icons/bs";
 
 const RegButton = () => {
   return (
     <AnimatedButton
-      text="Register Now"
-      className="flex w-fit items-center gap-x-2 rounded-full px-4 py-3"
+      text="Registration Closed"
+      className="flex w-fit cursor-not-allowed items-center gap-x-2 rounded-full px-4 py-3"
       id="register-button"
-      to="https://tally.so/r/wkrR1o"
-      target="_blank"
-    >
-      <BsArrowUpRightCircle className="text-[20px]" />
-    </AnimatedButton>
+    />
   );
 };
 
