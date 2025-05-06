897efbb0909970710f19ec926be13df9c6bdaf7a
Dinil Thilakarathne <126305123+Dinil-Thilakarathne@users.noreply.github.com>
2025-05-06 23:13:48 +0530

    style: enhance layout of ECertificatePage for better responsiveness

--- app/ECertificate/page.tsx
+++ app/ECertificate/page.tsx
@@ -2,7 +2,7 @@ import ECertificateForm from "@/components/ECertificate/ECertificateForm";
 
 const ECertificatePage = () => {
   return (
-    <div className="relative">
+    <div className="relative min-h-screen w-full overflow-scroll bg-black">
       <ECertificateForm />
     </div>
   );
