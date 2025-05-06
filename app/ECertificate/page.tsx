faf419f13eb5febd0cd8dd89d84e4c9367e6469b
Dinil Thilakarathne <126305123+Dinil-Thilakarathne@users.noreply.github.com>
2025-05-07 00:15:20 +0530

    style: add top-element class to enhance layout structure in ECertificatePage

--- app/ECertificate/page.tsx
+++ app/ECertificate/page.tsx
@@ -1,11 +1,13 @@
+import AnimatedGridWrapper from "@/components/AnimatedGrid/AnimatedGridWrapper";
 import ECertificateForm from "@/components/ECertificate/ECertificateForm";
 import HeroBgImage from "@/components/Hero/HeroBg";
 
 const ECertificatePage = () => {
   return (
     <div className="relative min-h-screen w-full overflow-scroll bg-black">
+      <AnimatedGridWrapper />
       <HeroBgImage />
-      <div className="relative flex min-h-screen w-full items-center justify-center">
+      <div className="top-element relative flex min-h-screen w-full items-center justify-center">
         <ECertificateForm />
       </div>
     </div>
