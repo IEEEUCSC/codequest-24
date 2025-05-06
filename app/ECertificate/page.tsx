7d9b2b71ed8a3ce712dda2a62813203c4674742c
Dinil Thilakarathne <126305123+Dinil-Thilakarathne@users.noreply.github.com>
2025-05-06 23:42:28 +0530

    feat: restructure ECertificatePage to include HeroBgImage and improve layout

--- app/ECertificate/page.tsx
+++ app/ECertificate/page.tsx
@@ -1,9 +1,13 @@
 import ECertificateForm from "@/components/ECertificate/ECertificateForm";
+import HeroBgImage from "@/components/Hero/HeroBg";
 
 const ECertificatePage = () => {
   return (
     <div className="relative min-h-screen w-full overflow-scroll bg-black">
-      <ECertificateForm />
+      <HeroBgImage />
+      <div className="relative flex min-h-screen w-full items-center justify-center">
+        <ECertificateForm />
+      </div>
     </div>
   );
 };
