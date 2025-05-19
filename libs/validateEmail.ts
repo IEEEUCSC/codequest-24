ff2267f27185251c4275539d09e767c90155a399
irushi716 <irushiweerasinghe716@gmail.com>
2025-05-20 05:01:35 +0530

    active participation commit01

--- libs/validateEmail.ts
+++ libs/validateEmail.ts
@@ -1,4 +1,5 @@
 import rawData from "../data/Both-Workshop-Participation.json";
+import rawDataActive from "../data/active_participation_without_position.json";
 
 export interface CertificateData {
   name: string;
@@ -7,11 +8,30 @@ export interface CertificateData {
   university: string;
 }
 
+export interface CertificateDataActive{
+  name: string;
+  email: string;
+  teamName: string;
+}
+
 interface RawEntry {
   "Name (First and Last)": string;
   "Email": string;
   "Contact Number (WhatsApp number)": string;
   "University / Institute": string;
+}
+
+interface RawEntryActive {
+  "Team Name": string | null;
+  "Number of Team Members": string | null;
+  "Full Name (Team Leader)": string | null;
+  "Email Address (Team Leader)": string | null;
+  "Full Name (Member 1)": string | null;
+  "Email Address (Member 1)": string | null;
+  "Full Name (Member 2)": string | null;
+  "Email Address (Member 2)": string | null;
+  "Full Name (Member 3)": string | null;
+  "Email Address (Member 3)": string | null;
 }
 
 export const validateEmailAndContact = async (
@@ -32,6 +52,51 @@ export const validateEmailAndContact = async (
           contactNumber: entry["Contact Number (WhatsApp number)"].trim(),
           university: entry["University / Institute"].trim(),
         };
+      }
+    }
+
+    return null;
+  } catch (error) {
+    console.error("Validation Error:", error);
+    return null;
+  }
+};
+
+
+export const validateEmailAndTeamName = async (
+  email: string,
+  teamName: string
+): Promise<CertificateDataActive | null> => {
+  try {
+    const dataActive: RawEntryActive[] = rawDataActive;
+
+    for (const entry of dataActive) {
+      if (entry["Team Name"]?.trim() === teamName.trim()) {
+        const emails = [
+          entry["Email Address (Team Leader)"],
+          entry["Email Address (Member 1)"],
+          entry["Email Address (Member 2)"],
+          entry["Email Address (Member 3)"],
+        ].map((e) => e?.trim().toLowerCase());
+
+        const matchedIndex = emails.findIndex(
+          (e) => e === email.trim().toLowerCase()
+        );
+
+        if (matchedIndex !== -1) {
+          const names = [
+            entry["Full Name (Team Leader)"],
+            entry["Full Name (Member 1)"],
+            entry["Full Name (Member 2)"],
+            entry["Full Name (Member 3)"],
+          ];
+
+          return {
+            name: names[matchedIndex]?.trim() ?? '',
+            email: email.trim(),
+            teamName: teamName.trim(),
+          };
+        }
       }
     }
 
