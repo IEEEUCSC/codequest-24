67cdc9c376ed2573777ff9e5da0a3825be1491d7
Dinil Thilakarathne <126305123+Dinil-Thilakarathne@users.noreply.github.com>
2025-06-09 02:18:43 +0530

    fix(timer): refactor countdown logic to use predefined end time and update heading text

--- app/timer/page.tsx
+++ app/timer/page.tsx
@@ -3,19 +3,9 @@ import CountdownTimer from "./CountDownTimer";
 import { BPMono } from "@/fonts";
 import HeroBgImage from "@/components/Hero/HeroBg";
 import Glassmorphism from "@/components/Glassmorphism";
+import { CRITICAL_HOURS, END_TIME } from "@/libs/data";
 
 export default function Page() {
-  const START_TIME = new Date("2025-05-14T09:36:00"); // Start time in UTC
-  const DURATION = 24; // Duration in hours
-  const CRITICAL_HOURS = 3; // Critical period in hours (e.g., 3 hours before the end)
-
-
-  const endTime = () => {
-    const date = START_TIME;
-    date.setHours(date.getHours() + DURATION);
-    return date;
-  };
-
   return (
     <main
       className={cn(
@@ -26,11 +16,11 @@ export default function Page() {
       <HeroBgImage />
 
       <Glassmorphism className="container mx-auto flex flex-col items-center justify-center rounded-2xl px-4 py-8 lg:gap-y-8 lg:p-16">
-        <h1 className="text-center text-lg font-bold text-slate-100 lg:text-6xl">
-          Race to Patch – Final Countdown
+        <h1 className="text-center text-lg font-bold text-slate-100 lg:text-4xl xl:text-6xl 2xl:text-7xl">
+          Final Countdown: The Race to Patch Begins
         </h1>
         <CountdownTimer
-          endTime={endTime()}
+          endTime={END_TIME}
           criticalHours={CRITICAL_HOURS}
           className="font-bpmono text-4xl"
           criticalClassName="text-primary-300 font-bold animate-ping"
